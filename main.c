62fe3b5290005946363e339c3b2a56d7f432a0c9
Your Name <you@example.com>
2019-01-15 19:27:38 +0800

    存钱系统框架

--- main.c
+++ main.c
@@ -1,4 +1,23 @@
 #include <stdio.h>
-int main(){
-    printf("进入存钱系统\n");
+int main()
+{
+    int qing=0;
+    printf("jinrucunqianxitong\n");
+    while(1)
+    {
+        printf("1---cunqian\n");
+        printf("2---qu qian\n");
+        printf("3---yu e\n");
+        printf("4---tui chu\n");
+        printf("shuruxiangyingderxuhao\n");
+        int code;
+        scanf("%d", &code);
+        if(code==1)
+        if(code==2)
+        if(code==3)
+        if(code==4)
+        break;
+    }
+    return 0;
+
 }
